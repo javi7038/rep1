3e4dc852e6d677edb5118e53a967910da15b4266
javi7038 <javi7038@gmail.com>
2024-01-18 12:18:31 -0400

    agregamos filtros a las tablas

---
+++ reportes.php
@@ -0,0 +1,560 @@
+<?php
+$jjjj ='
+{
+  "recordsTotal": 61,
+  "recordsFiltered": 61,
+  "data": [
+  {
+    "_id": "65a88b6af30fdad09965187a",
+    "index": 0,
+    "title": "Katheryn Bernard",
+    "descript": "Esse deserunt sit amet irure magna in pariatur deserunt duis.",
+    "creacion": "16 7 2017 T 11:56:01",
+    "modificacion": "20 2 2023 T 01:44:46",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6abf6bc5865ce9630b",
+    "index": 1,
+    "title": "Macdonald Waller",
+    "descript": "Pariatur duis sint aliqua enim ad.",
+    "creacion": "12 7 2023 T 07:58:37",
+    "modificacion": "23 2 2021 T 09:01:36",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6a44d01c46e61c2c80",
+    "index": 2,
+    "title": "Camacho Black",
+    "descript": "Esse occaecat nisi officia ipsum exercitation veniam reprehenderit labore.",
+    "creacion": "8 2 2016 T 03:12:21",
+    "modificacion": "27 8 2021 T 11:29:32",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6ad8bf3791db7ed12b",
+    "index": 3,
+    "title": "Tara Burgess",
+    "descript": "Culpa commodo anim veniam occaecat culpa in culpa do occaecat amet.",
+    "creacion": "23 12 2015 T 07:26:29",
+    "modificacion": "26 2 2014 T 01:48:32",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a6862a61bd618a866",
+    "index": 4,
+    "title": "Alston Vance",
+    "descript": "Pariatur voluptate tempor Lorem aliquip pariatur ea quis sunt.",
+    "creacion": "6 12 2016 T 05:59:23",
+    "modificacion": "16 9 2014 T 06:59:37",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a25edc7984e346a3d",
+    "index": 5,
+    "title": "Claire Harding",
+    "descript": "Dolor nisi cillum eu labore ea nulla irure voluptate duis cupidatat.",
+    "creacion": "5 11 2017 T 06:44:35",
+    "modificacion": "26 3 2021 T 12:16:35",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a2ba795e36cc23699",
+    "index": 6,
+    "title": "Kirkland Nguyen",
+    "descript": "Culpa incididunt sit et deserunt labore.",
+    "creacion": "17 10 2020 T 06:10:54",
+    "modificacion": "16 1 2016 T 04:14:17",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a5f0eb2fb4ba5ab04",
+    "index": 7,
+    "title": "Barry Miles",
+    "descript": "Et fugiat aute deserunt ex magna nostrud consequat eu dolore.",
+    "creacion": "2 12 2022 T 07:25:36",
+    "modificacion": "17 1 2020 T 01:47:28",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6aec7cdace9a2562fa",
+    "index": 8,
+    "title": "Ina Douglas",
+    "descript": "Dolor labore qui ex officia voluptate dolor duis tempor eiusmod.",
+    "creacion": "1 8 2018 T 05:31:30",
+    "modificacion": "22 5 2016 T 11:17:13",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6ad150f1e27060f59b",
+    "index": 9,
+    "title": "Newman Shepherd",
+    "descript": "Eu excepteur dolore ea nulla proident laborum sint ea in amet.",
+    "creacion": "30 7 2014 T 01:17:14",
+    "modificacion": "4 4 2021 T 03:05:00",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a38b773fce7464121",
+    "index": 10,
+    "title": "Marla Skinner",
+    "descript": "Nisi et consectetur sint dolor sint.",
+    "creacion": "26 1 2020 T 11:23:18",
+    "modificacion": "2 3 2022 T 04:28:33",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a72ef90f5ebb8fbee",
+    "index": 11,
+    "title": "Jacklyn Rush",
+    "descript": "Ut quis ad ipsum exercitation irure.",
+    "creacion": "22 10 2023 T 02:40:20",
+    "modificacion": "19 3 2015 T 05:42:03",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a42c528518930ddea",
+    "index": 12,
+    "title": "Shelly House",
+    "descript": "Veniam ipsum velit laboris consequat eu incididunt officia ea magna nisi cillum id.",
+    "creacion": "27 6 2018 T 10:56:30",
+    "modificacion": "13 9 2022 T 05:08:15",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6ac869fdcf436572eb",
+    "index": 13,
+    "title": "Williamson Hurley",
+    "descript": "Aute officia id nulla anim in reprehenderit sit aliquip laborum duis ipsum proident sint.",
+    "creacion": "31 1 2016 T 12:29:36",
+    "modificacion": "2 9 2016 T 09:38:41",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6ab5aa29cf5e2d49db",
+    "index": 14,
+    "title": "Camille Frank",
+    "descript": "Excepteur duis id ex ex tempor Lorem labore culpa.",
+    "creacion": "16 6 2015 T 03:17:17",
+    "modificacion": "24 2 2023 T 05:12:12",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a9588a36511122631",
+    "index": 15,
+    "title": "Chandler Reilly",
+    "descript": "Occaecat enim ipsum incididunt velit consectetur cupidatat adipisicing mollit incididunt dolore laborum fugiat ad.",
+    "creacion": "26 4 2020 T 02:19:30",
+    "modificacion": "14 11 2022 T 08:52:11",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a2a9b9acc1981c44b",
+    "index": 16,
+    "title": "Sherrie Nunez",
+    "descript": "Exercitation labore veniam veniam ea esse.",
+    "creacion": "24 8 2014 T 09:18:58",
+    "modificacion": "8 1 2019 T 01:48:27",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6ac617c6e282ffe2ad",
+    "index": 17,
+    "title": "Erna Wilkerson",
+    "descript": "Esse sint sit nulla enim sunt veniam aliqua.",
+    "creacion": "30 6 2023 T 04:58:30",
+    "modificacion": "1 2 2023 T 08:03:45",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a891e324c5c4a06d6",
+    "index": 18,
+    "title": "Terry Burke",
+    "descript": "Reprehenderit esse ullamco exercitation aute Lorem et exercitation nulla.",
+    "creacion": "16 8 2023 T 12:40:45",
+    "modificacion": "6 1 2015 T 07:03:23",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6a4d3b170390e11e79",
+    "index": 19,
+    "title": "Ladonna Becker",
+    "descript": "In qui eiusmod dolor excepteur et enim voluptate exercitation commodo esse.",
+    "creacion": "10 5 2015 T 05:02:18",
+    "modificacion": "30 3 2018 T 06:50:27",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6a455d657f7a0c74f3",
+    "index": 20,
+    "title": "Barrera Walters",
+    "descript": "Adipisicing sit consectetur qui dolore adipisicing exercitation.",
+    "creacion": "21 10 2015 T 09:06:01",
+    "modificacion": "31 5 2018 T 12:04:49",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6ae1cbf6e334da33a1",
+    "index": 21,
+    "title": "Felicia Snyder",
+    "descript": "Consectetur quis Lorem dolor laborum aute dolore et nostrud enim id.",
+    "creacion": "5 10 2023 T 03:58:06",
+    "modificacion": "19 10 2022 T 06:56:28",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6afc4e548cbdd15233",
+    "index": 22,
+    "title": "Key Fowler",
+    "descript": "Consectetur elit pariatur enim sit culpa.",
+    "creacion": "18 10 2021 T 03:46:28",
+    "modificacion": "17 2 2023 T 09:48:43",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a3bba5c363d21f757",
+    "index": 23,
+    "title": "Haynes Finch",
+    "descript": "Proident velit anim consectetur sint elit aute cupidatat mollit do dolore amet qui eu anim.",
+    "creacion": "10 12 2023 T 11:34:03",
+    "modificacion": "10 5 2021 T 12:38:32",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a3e8ca15eabd336dd",
+    "index": 24,
+    "title": "Monroe Beck",
+    "descript": "Sunt et dolor ea eu consequat ipsum ad quis aliqua officia aliquip dolore laborum commodo.",
+    "creacion": "13 4 2014 T 05:52:42",
+    "modificacion": "3 9 2022 T 06:25:48",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6a91e5304b83c5591a",
+    "index": 25,
+    "title": "Angelina Young",
+    "descript": "Consequat in sint veniam est adipisicing laboris occaecat enim ullamco in tempor irure tempor.",
+    "creacion": "1 8 2021 T 02:59:38",
+    "modificacion": "2 9 2014 T 10:24:46",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a6b2370fad41dab10",
+    "index": 26,
+    "title": "Kay Gregory",
+    "descript": "Nisi magna laborum ex ex voluptate incididunt qui nisi.",
+    "creacion": "1 12 2019 T 09:34:46",
+    "modificacion": "22 9 2023 T 07:24:41",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a0187fcd782059dd7",
+    "index": 27,
+    "title": "Spence Hawkins",
+    "descript": "Esse labore dolor elit sunt elit incididunt cupidatat laborum dolor velit esse ad nostrud sunt.",
+    "creacion": "16 5 2021 T 02:10:46",
+    "modificacion": "27 12 2014 T 10:17:08",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6ad4482c7823f74c4f",
+    "index": 28,
+    "title": "Thomas Stafford",
+    "descript": "Laboris dolor magna sunt magna sit elit commodo incididunt et veniam non laboris tempor.",
+    "creacion": "19 11 2020 T 07:54:27",
+    "modificacion": "17 10 2019 T 08:07:38",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6aed0ee86896c2b8a9",
+    "index": 29,
+    "title": "Church Lang",
+    "descript": "Quis ut Lorem aliqua adipisicing dolore esse sunt ipsum laborum culpa reprehenderit.",
+    "creacion": "10 3 2021 T 05:11:08",
+    "modificacion": "31 1 2015 T 06:51:33",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a257d3dcd0b7ae3c4",
+    "index": 30,
+    "title": "Charmaine Orr",
+    "descript": "Dolor ipsum excepteur duis esse magna aliquip dolor cillum.",
+    "creacion": "1 11 2017 T 09:47:06",
+    "modificacion": "3 12 2017 T 02:38:44",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a31a097f636d27708",
+    "index": 31,
+    "title": "Ortega Diaz",
+    "descript": "Aliquip id Lorem veniam veniam mollit nostrud ex anim.",
+    "creacion": "13 6 2014 T 02:31:39",
+    "modificacion": "19 5 2020 T 07:29:20",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a1a7c676836014ca8",
+    "index": 32,
+    "title": "Kenya Mcgowan",
+    "descript": "Cupidatat mollit quis ea veniam et tempor laboris.",
+    "creacion": "12 6 2020 T 09:33:53",
+    "modificacion": "16 9 2020 T 05:18:55",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6ae0cb81b063060d71",
+    "index": 33,
+    "title": "Mattie Mcleod",
+    "descript": "Eu sint ea est do ipsum incididunt enim.",
+    "creacion": "7 1 2018 T 02:19:21",
+    "modificacion": "30 3 2017 T 10:19:19",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a9f7eccb9d8838f16",
+    "index": 34,
+    "title": "Bobbie Goff",
+    "descript": "Ad minim aute nisi consequat magna eu ad qui.",
+    "creacion": "6 1 2017 T 09:27:16",
+    "modificacion": "19 2 2023 T 12:48:08",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a0e3874ac4cd24682",
+    "index": 35,
+    "title": "Vicky Acosta",
+    "descript": "Pariatur anim nulla proident officia officia voluptate consequat proident labore Lorem amet.",
+    "creacion": "10 9 2021 T 10:21:10",
+    "modificacion": "13 3 2020 T 06:13:32",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a58a238e3faadaba4",
+    "index": 36,
+    "title": "Leila Brady",
+    "descript": "Sit dolor laborum nisi cillum eiusmod magna mollit.",
+    "creacion": "1 1 2021 T 04:26:57",
+    "modificacion": "9 3 2023 T 07:11:52",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6ab9a4e395089966da",
+    "index": 37,
+    "title": "Erma Craft",
+    "descript": "Labore quis culpa voluptate aliquip irure culpa minim ullamco aute in ex.",
+    "creacion": "16 4 2022 T 05:17:46",
+    "modificacion": "8 2 2017 T 02:31:15",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a124967c63077151a",
+    "index": 38,
+    "title": "Morris Patterson",
+    "descript": "Dolore sint voluptate irure pariatur eiusmod ullamco dolore ut.",
+    "creacion": "8 3 2019 T 06:09:59",
+    "modificacion": "17 10 2019 T 05:37:39",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6a31fb9020c1c087ea",
+    "index": 39,
+    "title": "Josie Davenport",
+    "descript": "Aute ex magna proident elit officia id aliqua.",
+    "creacion": "10 6 2022 T 08:41:03",
+    "modificacion": "21 11 2020 T 08:47:36",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a8c9cf48e901ba54c",
+    "index": 40,
+    "title": "Lambert Pennington",
+    "descript": "Nisi consectetur sunt voluptate do.",
+    "creacion": "27 9 2016 T 08:57:48",
+    "modificacion": "6 5 2019 T 02:09:46",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a864b0acbe368e5d7",
+    "index": 41,
+    "title": "Hyde Chase",
+    "descript": "Do sunt anim ullamco culpa fugiat magna ad amet.",
+    "creacion": "30 12 2017 T 06:48:19",
+    "modificacion": "9 4 2018 T 05:24:15",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6a7eae1b6eb29f5786",
+    "index": 42,
+    "title": "Hall Merritt",
+    "descript": "Aute nulla fugiat eiusmod Lorem ad eiusmod dolore nostrud occaecat.",
+    "creacion": "4 4 2018 T 11:21:26",
+    "modificacion": "14 11 2018 T 11:30:27",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6ab6edd87164f3672c",
+    "index": 43,
+    "title": "Daisy Mcguire",
+    "descript": "In minim duis officia mollit et velit incididunt deserunt fugiat dolore ad occaecat.",
+    "creacion": "26 5 2023 T 03:42:53",
+    "modificacion": "28 8 2018 T 05:33:17",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a1d6d52572d324fef",
+    "index": 44,
+    "title": "Maryann Estes",
+    "descript": "Eu excepteur do occaecat elit officia eiusmod excepteur ullamco ad irure enim duis.",
+    "creacion": "31 7 2017 T 06:33:51",
+    "modificacion": "2 1 2019 T 05:33:48",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6a4b3f4e7ec54878e7",
+    "index": 45,
+    "title": "Day Lane",
+    "descript": "Sunt aliqua reprehenderit in in enim aute laboris excepteur excepteur ullamco veniam elit esse eiusmod.",
+    "creacion": "28 10 2019 T 11:02:17",
+    "modificacion": "11 2 2017 T 12:57:41",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6ac6b0b43df19f13ed",
+    "index": 46,
+    "title": "Savage Miranda",
+    "descript": "Laboris ex excepteur mollit tempor aute officia dolor.",
+    "creacion": "29 7 2017 T 01:05:56",
+    "modificacion": "26 6 2017 T 09:29:49",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a8b61c26a59449c40",
+    "index": 47,
+    "title": "Katrina Hart",
+    "descript": "Ut ut reprehenderit do labore et id sit eu tempor elit aute quis.",
+    "creacion": "25 3 2018 T 05:09:30",
+    "modificacion": "29 9 2023 T 10:58:31",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6aba61cc438d7d74ac",
+    "index": 48,
+    "title": "Glass Vang",
+    "descript": "Cillum est sit magna velit id occaecat occaecat minim anim dolore dolore veniam aute.",
+    "creacion": "30 11 2021 T 11:08:36",
+    "modificacion": "10 3 2018 T 04:24:36",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a29cb08d0b7149bd8",
+    "index": 49,
+    "title": "Tia Crane",
+    "descript": "Quis sit aliquip nulla cillum elit deserunt tempor.",
+    "creacion": "22 7 2023 T 10:02:27",
+    "modificacion": "22 7 2017 T 08:22:30",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6a160faa31ccd95a7b",
+    "index": 50,
+    "title": "Vaughn Best",
+    "descript": "Nostrud veniam nulla elit anim.",
+    "creacion": "1 1 2023 T 06:30:55",
+    "modificacion": "26 11 2014 T 05:25:21",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a907b5fe0545657fe",
+    "index": 51,
+    "title": "York Hendrix",
+    "descript": "Labore minim aliquip elit dolor.",
+    "creacion": "11 10 2022 T 01:54:18",
+    "modificacion": "31 1 2016 T 08:54:31",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a4cb9f8d26ddce680",
+    "index": 52,
+    "title": "Brittney Aguirre",
+    "descript": "Labore aute ut esse consequat in reprehenderit ex et minim adipisicing.",
+    "creacion": "3 12 2019 T 01:30:16",
+    "modificacion": "3 10 2019 T 01:36:48",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6ad3cd5679d9592cfa",
+    "index": 53,
+    "title": "Liz Guerra",
+    "descript": "Non ex voluptate aute occaecat nisi tempor aliqua voluptate voluptate excepteur amet magna.",
+    "creacion": "13 3 2021 T 11:46:19",
+    "modificacion": "13 11 2014 T 12:58:33",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6af9e24ad3ca1c9a3a",
+    "index": 54,
+    "title": "Pamela Pace",
+    "descript": "Do ad tempor qui dolore sint fugiat voluptate laborum et mollit culpa culpa occaecat.",
+    "creacion": "19 9 2022 T 10:45:21",
+    "modificacion": "24 12 2016 T 05:35:53",
+    "tipo": "blue"
+  },
+  {
+    "_id": "65a88b6a4802be22fd17fed4",
+    "index": 55,
+    "title": "Peggy Glover",
+    "descript": "Minim enim qui anim ex laboris quis labore.",
+    "creacion": "11 3 2017 T 11:26:08",
+    "modificacion": "21 9 2018 T 02:33:10",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a6a13d0a5dfe0188a",
+    "index": 56,
+    "title": "Beryl Vega",
+    "descript": "Sit qui proident labore qui pariatur aliqua dolor minim aliquip exercitation excepteur qui.",
+    "creacion": "24 2 2022 T 08:11:38",
+    "modificacion": "18 4 2017 T 11:57:09",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a15539274a859c5c3",
+    "index": 57,
+    "title": "Aileen Kirkland",
+    "descript": "Reprehenderit anim culpa dolore exercitation ullamco laboris magna ad id ea nostrud magna veniam.",
+    "creacion": "12 11 2021 T 07:31:07",
+    "modificacion": "11 5 2015 T 10:06:37",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6adf2f7b17290c0a52",
+    "index": 58,
+    "title": "Debra Melendez",
+    "descript": "Reprehenderit ipsum commodo non ullamco adipisicing fugiat.",
+    "creacion": "27 9 2017 T 04:00:22",
+    "modificacion": "29 5 2017 T 06:43:36",
+    "tipo": "green"
+  },
+  {
+    "_id": "65a88b6ae601618bcae6cf61",
+    "index": 59,
+    "title": "Leticia Ellis",
+    "descript": "Nulla anim anim dolore ut consequat enim reprehenderit cupidatat ex excepteur elit est fugiat sunt.",
+    "creacion": "18 9 2015 T 01:30:54",
+    "modificacion": "18 5 2020 T 01:03:11",
+    "tipo": "brown"
+  },
+  {
+    "_id": "65a88b6a8ca829abbe816ba5",
+    "index": 60,
+    "title": "Juliette Leon",
+    "descript": "Ad nulla cillum exercitation labore laborum aute nostrud id minim elit pariatur.",
+    "creacion": "22 7 2021 T 08:03:33",
+    "modificacion": "16 9 2021 T 07:40:01",
+    "tipo": "blue"
+  }
+]
+}';
+
+echo print($jjjj);
+?>
